e56d9fc87df3e02dd8056ba86715bc6997b1f854
jasmaa <jasonmaa3955@gmail.com>
2022-08-12 00:12:59 -0400

    Add gh-pages. Switch to relative pathing for gh-pages.

--- webpack.config.js
+++ webpack.config.js
@@ -5,58 +5,59 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 const port = process.env.PORT || 3000;
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    }),
-    new CopyPlugin({
-
-      patterns: [
-        // Use copy plugin to copy *.wasm to output folder.
-        { from: 'node_modules/onnxruntime-web/dist/*.wasm', to: '[name][ext]' },
-        { from: 'public/models', to: 'models/[name][ext]' },
-        { from: 'public/*.ico', to: '[name][ext]' },
-        { from: 'public/*.png', to: '[name][ext]' },
-        { from: 'public/*.json', to: '[name][ext]' },
-        { from: 'public/*.txt', to: '[name][ext]' },
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode,
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+            }
+          ]
+        }
       ]
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port: port,
-    historyApiFallback: true,
-    open: true
-  }
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          // Use copy plugin to copy *.wasm to output folder.
+          { from: 'node_modules/onnxruntime-web/dist/*.wasm', to: '[name][ext]' },
+          { from: 'public/models', to: 'models/[name][ext]' },
+          { from: 'public/*.ico', to: '[name][ext]' },
+          { from: 'public/*.png', to: '[name][ext]' },
+          { from: 'public/*.json', to: '[name][ext]' },
+          { from: 'public/*.txt', to: '[name][ext]' },
+        ]
+      }),
+    ],
+    devServer: {
+      host: 'localhost',
+      port: port,
+      historyApiFallback: true,
+      open: true
+    }
+  };
 };
